f63fa58c6d05e3434405296be3a944f07c10e2f9
jiaren <jia.ren7@gmail.com>
2022-03-26 12:30:25 -0700

    check in some corrections for info6205 midterm

--- src/neu/jia/info6205MidTerm/question03/ConstructPreOrderInOrderTree.java
+++ src/neu/jia/info6205MidTerm/question03/ConstructPreOrderInOrderTree.java
@@ -6,7 +6,9 @@ import java.util.Map;
 public class ConstructPreOrderInOrderTree {
 
     public TreeNode ConstructPreOrderInOrderTree(int[] inOrder, int[] preOrder) {
-        if (inOrder.length == 0) return null;
+        if (inOrder.length == 0) {
+            return null;
+        }
         Map<Integer, Integer> inOrderIndexMap = getIndexMapForInOrder(inOrder);
         return dfs(0, inOrder.length - 1, preOrder, 0, preOrder.length - 1, inOrderIndexMap);
     }
@@ -23,13 +25,17 @@ public class ConstructPreOrderInOrderTree {
     //dfs and return the construct tree
     private TreeNode dfs(int inStart, int inEnd, int[] preOrder, int preStart, int preEnd, Map<Integer, Integer> inMap) {
         //base
-        if (inStart > inEnd || preStart > preEnd) return null;
+        if (inStart > inEnd || preStart > preEnd) {
+            return null;
+        }
         int curHeadVal = preOrder[preStart];
         TreeNode curHead = new TreeNode(curHeadVal);
         int curHeadInOrderIdx = inMap.get(curHeadVal);
         int leftSubTreeLen = curHeadInOrderIdx - inStart;
+
         curHead.left = dfs(inStart, inStart + leftSubTreeLen - 1, preOrder, preStart + 1, preStart + leftSubTreeLen, inMap);
         curHead.right = dfs(curHeadInOrderIdx + 1, inEnd, preOrder, preStart + leftSubTreeLen + 1, preEnd, inMap);
+
         return curHead;
     }
 }
